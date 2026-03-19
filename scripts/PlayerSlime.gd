f93d2343cf9283e6a236201ac253212b4aae2b50
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-19 07:49:26 +0000

    slotchi_Interim_4

--- scripts/PlayerSlime.gd
+++ scripts/PlayerSlime.gd
@@ -3,7 +3,14 @@ extends AnimatedSprite2D
 
 
 ## UI Node References
-
+@onready var hunger_bar: ProgressBar = $UI/TopLeftContainer/TopLeft/HungerBar
+@onready var happiness_bar: ProgressBar = $UI/TopLeftContainer/TopLeft/HappinessBar
+@onready var multiplier_label: Label = $UI/TopLeftContainer/TopLeft/MultiplierLabel
+@onready var stats_label: Label = $UI/TopLeftContainer/TopLeft/StatusLabel
+@onready var timer_label: Label = $UI/TimerContainer/TimerLabel
+@onready var feed_button: Button = $UI/BottomBarContainer/BottomBar/FeedButton
+@onready var play_button: Button = $UI/BottomBarContainer/BottomBar/PlayButton
+@onready var train_button: Button = $UI/BottomBarContainer/BottomBar/TrainButton
 
 
 # Called when the node enters the scene tree for the first time.
