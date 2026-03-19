bec6e08cb24e584b88f2754bbb69b945ba2a90d7
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-19 22:51:22 +0000

    slotchi_Interim_6

--- scripts/AgilityMiniGame.gd
+++ scripts/AgilityMiniGame.gd
@@ -1,11 +1,69 @@
+### AgilityMiniGame.gd
 extends Control
 
+## UI Nodes
+@onready var prompt_label: Label = $PromptLabel
+@onready var timer_bar: ProgressBar = $TimerBar
+@onready var result_label: Label = $ResultLabel
+@onready var round_label: Label = $RoundLabel
+@onready var back_button: Button = $BackButton
 
-# Called when the node enters the scene tree for the first time.
+## Round Settings
+var total_rounds: int = 5
+var current_round: int = 0
+var successes: int = 0
+var round_time: float = 1.5
+var round_timer: float = 0.0
+var is_active: bool = false
+var waiting_for_input: bool = false
+
+# Arrow Directions
+var directions: Array = ["UP", "DOWN", "LEFT", "RIGHT"]
+var current_direction: String = ""
+var direction_actions: Dictionary ={
+	"UP" : "ui_up",
+	"DOWN" : "ui_down",
+	"LEFT" : "ui_left",
+	"RIGHT" : "ui_right"
+}
+
+
+
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	pass
+	
+	
+func _input(event: InputEvent) -> void:
+	
+	
+
+func _start_round() -> void:
+	
+
+
+func _next_round() -> void:
+	
+	
+
+func _finish_minigame() -> void:
+	
+	
+	
+func _on_back_pressed() -> void:
+	GameManager.change_scene("res://scenes/care_room.tscn", GameManager.GameState.CARE)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
