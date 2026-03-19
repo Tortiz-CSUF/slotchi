e17b13e4898eb82add6f7ee2076baf1622ade93b
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-19 00:53:56 +0000

    laser-mirror-puzzle-Interim_1

--- scripts/MonsterData.gd
+++ scripts/MonsterData.gd
@@ -1,11 +1,48 @@
+### MonsterData.gd
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+## Autoload singleton that stores all persistent monster stats.
+## Hunger/ Happiness will drain  overtime and affect trainging multipliers.
 
+## Status Bars (0 - 100)
+var hunger: float = 50.0
+var happiness: float = 50.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+## Combat Stats
+var atk: int = 5
+var def: int = 5
+var max_hp: int = 30
+var current_hp: int = 30
+
+## Status Drain Rate (points per sec)
+var drain_rate: float = 1.0
+
+# Feed/Play Increment Amt
+var feed_amount: float = 20.0 
+var play_amount: float = 20.0
+
+## Calcs the training multiplier based on current stats.
+## Each bar above 75% will grant a 1.5x multi.
+func get_training_multiplier() -> float:
+	var multiplier: float = 1.0
+	if hunger > 75.0:
+		multiplier *= 1.5
+	if happiness > 75.0:
+		multiplier *= 1.5
+	return multiplier
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
