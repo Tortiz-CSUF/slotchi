0ffb2e8f46cd5e964c1b781dfce42e6379e92d77
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-19 08:29:37 +0000

    slotchi_Interim_9

--- scripts/StrengthMiniGame.gd
+++ scripts/StrengthMiniGame.gd
@@ -1,7 +1,24 @@
+### StrengthMiniGame.gd
 extends Control
 
+## Node Refs
+@onready var power_bar: ProgressBar = $PowerBar
+@onready var marker: ColorRect = $Marker
+@onready var target_zone: ColorRect = $TargetZone
+@onready var result_label: Label = $ResultLabel
+@onready var back_button: Button = $BackButton
 
-# Called when the node enters the scene tree for the first time.
+
+## Marker Movement
+var marker_speed: float = 400.0			# pixels per sec
+var marker_direction: int = 1 			# 1 = right, -1 = left
+var bar_start_x: float = 0.0
+var bar_end_x: float = 0.0
+var is_active: bool = true				# true: mini game in progress
+
+
+
+
 func _ready() -> void:
 	pass # Replace with function body.
 
