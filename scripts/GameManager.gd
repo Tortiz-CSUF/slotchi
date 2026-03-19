8ee456fd92a1d617a7eae9a535fb0db18cb03e38
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-19 01:09:00 +0000

    laser-mirror-puzzle-Interim_3

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,11 +1,25 @@
+### GameData.gd
 extends Node
 
+## Autoload singleton that manages the battle timer, game state, and scene trans.
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+## BattleTimer (secs)
+var battle_timer: float = 180.0
+var timer_running: bool = false
+
+## Game State Tracking
+enum GameState { MENU, CARE, TRAINING, BATTLE }
+var current_state: int = GameState.MENU
+
+## Signals
+signal battle_timer_expired				## emits when battle timer reaches zero
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func update_battle_timer(delta: float) -> void:
+	if timer_running and current_state == GameState.CARE:
+		battle_timer -= delta
+		if battle_timer <= 0.0:
+			battle_timer = 0.0 
+			timer_running = false
+			battle_timer_expired.emit()
