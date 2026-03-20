c617dee42a6100c388ceb2b294bb4e842c5c32f2
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-20 00:38:36 +0000

    slotchi_Interim_6

--- scripts/BattleArena.gd
+++ scripts/BattleArena.gd
@@ -1,4 +1,19 @@
+### BattleArena.gd
 extends Control
+
+## UI Nodes
+@onready var player_hp_bar: ProgressBar = $PlayerSideContainer/PlayerSide/PlayerHPBar
+@onready var enemy_hp_bar: ProgressBar = $EnemySideContainer/EnemySide/EnemyHPBar
+@onready var intent_icon: Label = $EnemySideContainer/EnemySide/IntentIcon
+@onready var slot_grid: GridContainer = $SlotGrid
+@onready var card1: Button = $CardContainer/Card/Card1
+@onready var card2: Button = $CardContainer/Card/Card2
+@onready var card3: Button = $CardContainer/Card/Card3
+@onready var spin_button: Button = $SpinButton
+@onready var info_label: Label = $InfoLabel
+@onready var end_panel: ColorRect = $EndPanel
+@onready var end_label: Label = $EndPanel/EndLabel
+@onready var return_button: Button = $EndPanel/ReturnButton
 
 
 # Called when the node enters the scene tree for the first time.
