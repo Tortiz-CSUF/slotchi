581371e1604cb2479828d488087541ca16655c27
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-20 03:16:58 +0000

    slotchi_Interim_1

--- scripts/BattleArena.gd
+++ scripts/BattleArena.gd
@@ -15,6 +15,35 @@ extends Control
 @onready var end_label: Label = $EndPanel/EndLabel
 @onready var return_button: Button = $EndPanel/ReturnButton
 
+## Icon Textures
+var sword_icon: Texture2D = preload("res://assets/sprites/icons/sword.png")
+var shield_icon: Texture2D = preload("res://assets/sprites/icons/shield.png")
+var heart_icon: Texture2D = preload("res://assets/sprites/icons/heart.png")
+var skull_icon: Texture2D = preload("res://assets/sprites/icons/skull.png")
+
+## Symbol Pool
+var symbol_pool: Array = []
+var base_pool: Array = []
+
+## Enemy State
+var enemy_max_hp: int = 50
+var enemy_hp: int = 50
+var enemy_intent: String = "" 		## attack or sabotage
+
+## Spin Sate
+var is_spinning: bool = false
+var spin_timer: float = 0.0
+var spin_duration: float = 1.5
+var spin_interval: float = 0.05			## amt slots randomzie
+var spin_tick: float = 0.0
+
+## Turn State
+var card_selected: bool = false
+var battle_over: bool = false
+
+## Refs to slot textures
+var slots: Array = []
+
 
 
 func _ready() -> void:
